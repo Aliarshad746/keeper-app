0906acc058f388089fe2cfc249f8ceff1bbef225
arshad <aliarshad746@gmail.com>
2020-08-22 15:23:33 +0530

    first commit.

---
+++ build/precache-manifest.633640f336b0f5b110f5acebca1e3cab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7ba2be3267cf3fc0e6c0ec6321d8c9",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "ef4386f7dc9d3295b9a3",
+    "url": "/keeper-app/static/js/2.e8063a4a.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/keeper-app/static/js/2.e8063a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e0baf5ad4c6df9f2e1",
+    "url": "/keeper-app/static/js/main.9ba2d2de.chunk.js"
+  },
+  {
+    "revision": "9efce025e1cc176d2de9",
+    "url": "/keeper-app/static/js/runtime-main.d410b0b7.js"
+  }
+]);
